acec3fcce9bc61028ebc252b33b92117d10e290c
gremlin961 <richard.kiles@gmail.com>
2022-08-30 13:49:24 -0400

    first pass breaking out the script

--- temperature.py
+++ temperature.py
@@ -1,10 +1,10 @@
 import board
 import adafruit_dht
 
-def get():
-    dhtDevice1 = adafruit_dht.DHT22(board.D4, use_pulseio=False)
-    temperature_f = dhtDevice1.temperature * 1.8 + 32
-    humidity = dhtDevice1.humidity
+dhtDevice1 = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-    return temperature_f
-    return humidity
+def temp():
+    temperature_f = dhtDevice1.temperature * 1.8 + 32
+
+def humidity():
+    humidity = dhtDevice1.humidity
