330093ebf2cf8cd0f43ff706dc02cf08029f112f
gremlin961 <richard.kiles@gmail.com>
2022-09-21 13:33:48 -0400

    Docerkized the web server

---
+++ rvstat-web/app/views.py
@@ -0,0 +1,42 @@
+from app import app
+import os
+from google.cloud import storage
+from google.cloud import datastore
+#import json
+import tempfile
+from flask import render_template
+#from app import gcsdata
+
+IMG_FOLDER = os.path.join('static', 'images')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
+
+def download_blob(bucket_name, source_file, destination_file):
+    """Downloads a blob from the bucket."""
+    # The ID of your GCS bucket
+    bucket_name = bucket_name
+    # The ID of your GCS object
+    source_file = source_file
+    # The path to which the file should be downloaded
+    destination_file = destination_file
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(source_file)
+    blob.download_to_filename(destination_file)
+
+
+@app.route('/')
+def home():
+    client = datastore.Client()
+    reading_key = client.key("Reading", "environmental")
+    query = client.get(reading_key)
+    temp = query['temp']
+    humidity = query['humidity']
+    date = query['date']
+    time = query['time']
+    image_down = download_blob('rvstat-dev', 'image-up.jpg', '/home/richard_kiles/rvstat-web/app/static/images/image.jpg')
+    image = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
+
+    return render_template('home.html', temp=temp, humidity=humidity, date=date, time=time, image=image)
